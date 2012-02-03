fbd1c90af640a4ea90d99a912a40a0e4eba6e06c
shenry <shenry@Proxiadev83.PROXIAD.NORD>
2012-02-03 11:47:52 +0100

    premier

---
+++ HelloGoogleMaps/src/TestGoogleMap/HelloGoogleMaps.java
@@ -0,0 +1,21 @@
+package TestGoogleMap;
+
+
+import com.google.android.maps.MapActivity;
+//import com.google.android.maps.MapView;
+import Test.Google.Map.R;
+import android.os.Bundle;
+
+public class HelloGoogleMaps extends MapActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+/*		MapView test = (MapView) findViewById(R.id.mapview);
+		test.setBuiltInZoomControls(true);
+*/	}
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+}
