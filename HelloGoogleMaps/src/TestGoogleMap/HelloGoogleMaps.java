d9642a70795fe56637e03a2061ae363f9c8011a3
henry <s.henry@proxiad.com>
2012-02-03 16:26:21 +0100

    Update HelloGoogleMaps/src/TestGoogleMap/HelloGoogleMaps.java Conflicts:
    	HelloGoogleMaps/src/TestGoogleMap/HelloGoogleMaps.java
    
    Signed-off-by: henry <s.henry@proxiad.com>

--- HelloGoogleMaps/src/TestGoogleMap/HelloGoogleMaps.java
+++ HelloGoogleMaps/src/TestGoogleMap/HelloGoogleMaps.java
@@ -6,7 +6,6 @@ import com.google.android.maps.MapActivity;
 import Test.Google.Map.R;
 import android.os.Bundle;
 
-
 public class HelloGoogleMaps extends MapActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -19,21 +18,6 @@ public class HelloGoogleMaps extends MapActivity {
     protected boolean isRouteDisplayed() {
         return false;
     }
-
-
-/*test de rapatriement : Test Ok*/
-
-/*voici la version de la branche 1 pour le moment en local*/
-
-	public static final String TABLE_NAME = "test branche1";
-
-/*voici la version de la branche 1 pour le moment en local*/
-
-/*voici la version de la branche 2 pour le moment en local*/
-
-	public static final String Test_Branche = "test branche2";
-
-/*voici la version de la branche 2 pour le moment en local*/
-	
 }
 
+/*test de rapatriement*/
